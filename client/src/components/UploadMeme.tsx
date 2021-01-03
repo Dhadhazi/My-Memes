4ae24d9544474b3c7987e7bc8e7bf2f55d262c03
Dhadhazi <dhadhazi@gmail.com>
2021-01-03 08:17:29 +0100

    Client upload form modified

--- client/src/components/UploadMeme.tsx
+++ client/src/components/UploadMeme.tsx
@@ -3,11 +3,16 @@ import { getUploadUrl } from "../api/memeapi";
 
 export const UploadMeme = () => {
   const [file, setFile] = useState<any>();
+  const [category, setCategory] = useState<string>();
 
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.target.files;
     if (!files) return;
     setFile(files[0]);
+  }
+
+  function handleCategoryChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setCategory(e.target.value);
   }
 
   async function handleSubmit(event: React.SyntheticEvent) {
@@ -17,19 +22,32 @@ export const UploadMeme = () => {
       alert("File should be selected!");
       return;
     }
+
+    console.log(event);
   }
 
   return (
     <div>
-      <h3>Upload new meme</h3>
+      <h3>Add New Meme</h3>
       <form onSubmit={handleSubmit}>
+        Add a category:{" "}
+        <input
+          type="text"
+          name="category"
+          placeholder="Category"
+          onChange={handleCategoryChange}
+          value={category}
+        />
+        <br />
         <label>File: </label>
         <input
+          name="file"
           type="file"
           accept="image/*"
           placeholder="Image to upload"
           onChange={handleFileChange}
         />
+        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
